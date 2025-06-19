0d2f9603052bbb7ce76681d10f1eab157fc9550d
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2025-06-20 04:29:52 +0800

    Initial commit

---
+++ mesecore_cobblestone.lua
@@ -0,0 +1,10 @@
+core.register_craft("camomese:mesecore_cobblestone", {
+    description = "Mesecore Cobblestone",
+    inventory_image = "camomese_mesecore_cobblestone.png"
+})
+
+core.register_node("camomese:mesecore_cobblestone", {
+    description = "Mesecore Cobblestone",
+    tiles = {"camomese_mesecore_cobblestone.png"},
+    groups = {cracky=3, stone=1}
+})
