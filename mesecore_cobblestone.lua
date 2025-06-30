2def68de792613c6f3a8f34f7a969b9f26e5d8be
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2025-07-01 03:06:34 +0800

    Added more detail for the node

--- mesecore_cobblestone.lua
+++ mesecore_cobblestone.lua
@@ -1,10 +1,37 @@
-core.register_craft("camomese:mesecore_cobblestone", {
-    description = "Mesecore Cobblestone",
-    inventory_image = "camomese_mesecore_cobblestone.png"
+core.register_node("camomese:mesecore_cobblestone", {
+    description = S("Mese Core Cobblestone"),
+    tiles = {"camomese_mesecore_cobblestone.png"},
+	is_ground_content = false,
+	inventory_image = minetest.inventorycube("camomese_mesecore_cobblestone.png"),
+	groups = {cracky = 3},
+	sounds = mesecon.node_sound.stone,
+	mesecons = {conductor = {
+		state = mesecon.state.off,
+		rules = mesecon.rules.alldirs,
+		onstate = "camomese:mesecore_cobblestone_on"
+	}},
+	on_blast = mesecon.on_blastnode,
 })
 
-core.register_node("camomese:mesecore_cobblestone", {
-    description = "Mesecore Cobblestone",
-    tiles = {"camomese_mesecore_cobblestone.png"},
-    groups = {cracky=3, stone=1}
+core.register_node("camomese:mesecore_cobblestone_on", {
+    description = S("Mese Core Cobblestone"),
+    tiles = {"camomese_mesecore_cobblestone_on.png"},
+	is_ground_content = false,
+	groups = {cracky = 3},
+	sounds = mesecon.node_sound.stone,
+	mesecons = {conductor = {
+		state = mesecon.state.on,
+		rules = mesecon.rules.alldirs,
+		offstate = "camomese:mesecore_cobblestone"
+	}},
+	on_blast = mesecon.on_blastnode,
+})
+
+core.register_craft({
+	output = "camomese:mesecore_cobblestone 8",
+	recipe = {
+		{"default:cobblestone", "default:cobblestone", "default:cobblestone"},
+		{"default:cobblestone", "default:mese_crystal_fragment", "default:cobblestone"},
+		{"default:cobblestone", "default:cobblestone", "default:cobblestone"},
+	}
 })
