75150beaa478a32dfa27db31743d07615ac0445b
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2025-07-01 03:06:47 +0800

    Initial commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Camoflaged Mesecons (camomese) Mod by WoGoMo
+
+-- Translation support
+
+local S = core.get_translator("camomese")
+local path = core.get_modpath(core.get_current_modname()) .. "/"
+
+-- add blocks
+dofile(path .. "mesecore_cobblestone.lua")
+
+print("[MOD] Camomese loaded")
