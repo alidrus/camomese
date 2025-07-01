cfdb6315b1b34b0469d1f0927231803024522124
Syed Abu Bakar Mashor Al-Idrus <alidrus@xdms.com>
2025-07-01 10:52:59 +0800

    Changed name from mesecore_cobblestone to mesecore_cobble (following mtg default convention)

--- mesecore_cobblestone.lua
+++ mesecore_cobblestone.lua
@@ -3,37 +3,37 @@ local S = minetest.get_translator("camomese")
 
 
 -- Register the Mese Core Cobblestone (off state) node
-minetest.register_node("camomese:mesecore_cobblestone", {
+minetest.register_node("camomese:mesecore_cobble", {
     description = S("Mese Core Cobblestone"),
-    tiles = {"camomese_mesecore_cobblestone.png"},
+    tiles = {"camomese_mesecore_cobble.png"},
 	is_ground_content = false,
-	inventory_image = minetest.inventorycube("camomese_mesecore_cobblestone.png"),
+	inventory_image = minetest.inventorycube("camomese_mesecore_cobble.png"),
 	groups = {cracky = 3},
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {conductor = {
 		state = mesecon.state.off,
 		rules = mesecon.rules.alldirs,
-		onstate = "camomese:mesecore_cobblestone_on"
+		onstate = "camomese:mesecore_cobble_on"
 	}},
 	on_blast = mesecon.on_blastnode,
 })
 
 -- Register the Mese Core Cobblestone (on state) node
-minetest.register_node("camomese:mesecore_cobblestone_on", {
-    tiles = {"camomese_mesecore_cobblestone_on.png"},
+minetest.register_node("camomese:mesecore_cobble_on", {
+    tiles = {"camomese_mesecore_cobble_on.png"},
 	is_ground_content = false,
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {conductor = {
 		state = mesecon.state.on,
 		rules = mesecon.rules.alldirs,
-		offstate = "camomese:mesecore_cobblestone"
+		offstate = "camomese:mesecore_cobble"
 	}},
 	on_blast = mesecon.on_blastnode,
 })
 
 -- Register the recipe
 minetest.register_craft({
-	output = "camomese:mesecore_cobblestone 8",
+	output = "camomese:mesecore_cobble 8",
 	recipe = {
 		{"default:cobble", "default:cobble", "default:cobble"},
 		{"default:cobble", "default:mese_crystal_fragment", "default:cobble"},
